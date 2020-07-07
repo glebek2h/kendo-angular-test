a9bc6e2ae347030f8a6afc37536b5dd4bb6949f4
Gleb.Kazachynski <Gleb.Kazachynski@wolterskluwer.com>
2020-07-07 19:01:48 +0300

    minor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {GridModule, SharedModule} from '@progress/kendo-angular-grid';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule,
+    BrowserAnimationsModule,
+    GridModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products.ts
@@ -0,0 +1,383 @@
+export const sampleProducts = [
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "aDemoToday",
+    "SupplierID": 1,
+    "CategoryID": 1,
+    "QuantityPerUnit": "10 boxes x 20 bags",
+    "UnitPrice": "Add",
+    "UnitsInStock": 39,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 10,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 1,
+      "CategoryName": "Beverages",
+      "Description": "Soft drinks, coffees, teas, beers, and ales"
+    },
+    "FirstOrderedOn": new Date(1996, 8, 20)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "AETNA",
+    "SupplierID": 1,
+    "CategoryID": 1,
+    "QuantityPerUnit": "24 - 12 oz bottles",
+    "UnitPrice": "Add",
+    "UnitsInStock": 17,
+    "UnitsOnOrder": 40,
+    "ReorderLevel": 25,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 1,
+      "CategoryName": "Beverages",
+      "Description": "Soft drinks, coffees, teas, beers, and ales"
+    },
+    "FirstOrderedOn": new Date(1996, 7, 12)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "aLRWtest",
+    "SupplierID": 1,
+    "CategoryID": 2,
+    "QuantityPerUnit": "12 - 550 ml bottles",
+    "UnitPrice": "Add",
+    "UnitsInStock": 13,
+    "UnitsOnOrder": 70,
+    "ReorderLevel": 25,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 8, 26)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "ANTHEM OHAS",
+    "SupplierID": 2,
+    "CategoryID": 2,
+    "QuantityPerUnit": "48 - 6 oz jars",
+    "UnitPrice": "Add",
+    "UnitsInStock": 53,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 0,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 9, 19)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "aTestFactorEntry",
+    "SupplierID": 2,
+    "CategoryID": 2,
+    "QuantityPerUnit": "36 boxes",
+    "UnitPrice": "Add",
+    "UnitsInStock": 0,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 0,
+    "Discontinued": true,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 7, 17)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "CIGNA HMO",
+    "SupplierID": 3,
+    "CategoryID": 2,
+    "QuantityPerUnit": "12 - 8 oz jars",
+    "UnitPrice": "Add",
+    "UnitsInStock": 120,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 25,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 9, 19)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "CIGNA PPO",
+    "SupplierID": 3,
+    "CategoryID": 7,
+    "QuantityPerUnit": "12 - 1 lb pkgs.",
+    "UnitPrice": "Add",
+    "UnitsInStock": 15,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 10,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 7,
+      "CategoryName": "Produce",
+      "Description": "Dried fruit and bean curd"
+    },
+    "FirstOrderedOn": new Date(1996, 7, 22)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "KHA MANAGED CARE",
+    "SupplierID": 3,
+    "CategoryID": 2,
+    "QuantityPerUnit": "12 - 12 oz jars",
+    "UnitPrice": "Add",
+    "UnitsInStock": 6,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 0,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 11, 1)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "KHA MEDICAID",
+    "SupplierID": 4,
+    "CategoryID": 6,
+    "QuantityPerUnit": "18 - 500 g pkgs.",
+    "UnitPrice": "Add",
+    "UnitsInStock": 29,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 0,
+    "Discontinued": true,
+    "Category": {
+      "CategoryID": 6,
+      "CategoryName": "Meat/Poultry",
+      "Description": "Prepared meats"
+    },
+    "FirstOrderedOn": new Date(1997, 1, 21)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "KHA MEDICARE",
+    "SupplierID": 4,
+    "CategoryID": 8,
+    "QuantityPerUnit": "12 - 200 ml jars",
+    "UnitPrice": "Add",
+    "UnitsInStock": 31,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 0,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 8,
+      "CategoryName": "Seafood",
+      "Description": "Seaweed and fish"
+    },
+    "FirstOrderedOn": new Date(1996, 8, 5)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "aDemoToday",
+    "SupplierID": 1,
+    "CategoryID": 1,
+    "QuantityPerUnit": "10 boxes x 20 bags",
+    "UnitPrice": "Add",
+    "UnitsInStock": 39,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 10,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 1,
+      "CategoryName": "Beverages",
+      "Description": "Soft drinks, coffees, teas, beers, and ales"
+    },
+    "FirstOrderedOn": new Date(1996, 8, 20)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "AETNA",
+    "SupplierID": 1,
+    "CategoryID": 1,
+    "QuantityPerUnit": "24 - 12 oz bottles",
+    "UnitPrice": "Add",
+    "UnitsInStock": 17,
+    "UnitsOnOrder": 40,
+    "ReorderLevel": 25,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 1,
+      "CategoryName": "Beverages",
+      "Description": "Soft drinks, coffees, teas, beers, and ales"
+    },
+    "FirstOrderedOn": new Date(1996, 7, 12)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "aLRWtest",
+    "SupplierID": 1,
+    "CategoryID": 2,
+    "QuantityPerUnit": "12 - 550 ml bottles",
+    "UnitPrice": "Add",
+    "UnitsInStock": 13,
+    "UnitsOnOrder": 70,
+    "ReorderLevel": 25,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 8, 26)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "ANTHEM OHAS",
+    "SupplierID": 2,
+    "CategoryID": 2,
+    "QuantityPerUnit": "48 - 6 oz jars",
+    "UnitPrice": "Add",
+    "UnitsInStock": 53,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 0,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 9, 19)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "aTestFactorEntry",
+    "SupplierID": 2,
+    "CategoryID": 2,
+    "QuantityPerUnit": "36 boxes",
+    "UnitPrice": "Add",
+    "UnitsInStock": 0,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 0,
+    "Discontinued": true,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 7, 17)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "CIGNA HMO",
+    "SupplierID": 3,
+    "CategoryID": 2,
+    "QuantityPerUnit": "12 - 8 oz jars",
+    "UnitPrice": "Add",
+    "UnitsInStock": 120,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 25,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 9, 19)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "CIGNA PPO",
+    "SupplierID": 3,
+    "CategoryID": 7,
+    "QuantityPerUnit": "12 - 1 lb pkgs.",
+    "UnitPrice": "Add",
+    "UnitsInStock": 15,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 10,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 7,
+      "CategoryName": "Produce",
+      "Description": "Dried fruit and bean curd"
+    },
+    "FirstOrderedOn": new Date(1996, 7, 22)
+  },
+];
+
+export const products = [
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "aDemoToday",
+    "SupplierID": 1,
+    "CategoryID": 1,
+    "QuantityPerUnit": "10 boxes x 20 bags",
+    "UnitPrice": "Add",
+    "UnitsInStock": 39,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 10,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 1,
+      "CategoryName": "Beverages",
+      "Description": "Soft drinks, coffees, teas, beers, and ales"
+    },
+    "FirstOrderedOn": new Date(1996, 8, 20)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "AETNA",
+    "SupplierID": 1,
+    "CategoryID": 1,
+    "QuantityPerUnit": "24 - 12 oz bottles",
+    "UnitPrice": "Add",
+    "UnitsInStock": 17,
+    "UnitsOnOrder": 40,
+    "ReorderLevel": 25,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 1,
+      "CategoryName": "Beverages",
+      "Description": "Soft drinks, coffees, teas, beers, and ales"
+    },
+    "FirstOrderedOn": new Date(1996, 7, 12)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "aLRWtest",
+    "SupplierID": 1,
+    "CategoryID": 2,
+    "QuantityPerUnit": "12 - 550 ml bottles",
+    "UnitPrice": "Add",
+    "UnitsInStock": 13,
+    "UnitsOnOrder": 70,
+    "ReorderLevel": 25,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 8, 26)
+  },
+  {
+    "ProductID": '12/31/2024',
+    "ProductName": "ANTHEM OHAS",
+    "SupplierID": 2,
+    "CategoryID": 2,
+    "QuantityPerUnit": "48 - 6 oz jars",
+    "UnitPrice": "Add",
+    "UnitsInStock": 53,
+    "UnitsOnOrder": 0,
+    "ReorderLevel": 0,
+    "Discontinued": false,
+    "Category": {
+      "CategoryID": 2,
+      "CategoryName": "Condiments",
+      "Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+    },
+    "FirstOrderedOn": new Date(1996, 9, 19)
+  },
+  ];
